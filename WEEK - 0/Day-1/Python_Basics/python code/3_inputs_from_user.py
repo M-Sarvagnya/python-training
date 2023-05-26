b2ac65432e42de13647ce7845b065a6a1d8c477b
M-Sarvagnya <134617671+M-Sarvagnya@users.noreply.github.com>
2023-05-26 08:07:37 +0530

    Update 3_inputs_from_user.py

--- WEEK - 0/Day-1/Python_Basics/python code/3_inputs_from_user.py
+++ WEEK - 0/Day-1/Python_Basics/python code/3_inputs_from_user.py
@@ -1,31 +1,3 @@
-'''try:
-    a = input("Enter a value: ")
-    b = int(input("Enter b value: "))
-    c = float(input("Enter c value: "))
-except:
-    print("In-correct sequence")
-else:
-    print("correct sequence")'''   
-
-'''try:
-    a = input("Enter a value: ")
-except:
-    print("In-correct a sequence")
-else:
-    print("correct a sequence")
-try:
-    b = int(input("Enter b value: "))
-except:
-    print("In-correct sequence")
-else:
-    print("correct sequence") 
-try:
-    c = float(input("Enter c value: "))
-except:
-    print("In-correct sequence")
-else:
-    print("correct sequence")'''
-
 try:
     a = input("Enter a value: ")
     b = int(input("Enter b value: "))
